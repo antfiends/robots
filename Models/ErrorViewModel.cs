d89bef08c05fa2d3df6ecb426bb9b93319484e8d
antfriend <antfriend@gmail.com>
2023-12-20 12:15:57 -0700

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace robots.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
